f47ce36d11f6a94745d0d3d98cff0efd857913d7
Natalia <natalia_bochenek@hotmail.com>
2021-03-22 19:17:45 +0100

    add footer

--- travelAgency-natalia/travelAgencyNat/src/app/app.module.ts
+++ travelAgency-natalia/travelAgencyNat/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { AboutPageComponent } from './about-page/about-page.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { HeroComponent } from './hero/hero.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HeroComponent } from './hero/hero.component';
     NavbarComponent,
     AboutPageComponent,
     HomePageComponent,
-    HeroComponent
+    HeroComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
